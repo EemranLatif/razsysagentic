43d27923f894042b049b5251f8978b8c326edebc
Imran Latif <imranlatifpro@Imrans-MacBook-Pro-2.local>
2026-06-27 14:42:50 -0400

    push file with send grid

--- personal/sendgrd.py
+++ personal/sendgrd.py
@@ -1,6 +1,8 @@
 import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+from dotenv import load_dotenv	
+load_dotenv(override=True)
 
 message = Mail(
 	from_email="imranlatifclouds@gmail.com",
